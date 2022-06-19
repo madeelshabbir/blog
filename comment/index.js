57e2e2ef409299db7c1c94a09e5a799fa969a814
Adeel Shabbir <madeelshabbirs@gmail.com>
2022-06-19 22:44:29 +0500

    get and post APIs for /posts/:id/comments

---
+++ comment/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+
+const comments = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(comments[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', bodyParser.json(), (req, res) => {
+  const postComments = comments[req.params.id] || [];
+  const comment = {
+    id: randomBytes(4).toString('hex'),
+    content: req.body.content
+  };
+
+  postComments.push(comment);
+  comments[req.params.id] = postComments;
+  res.status(201).send(comment);
+});
+
+app.listen(3002, () => {
+  console.log('Server running at port 3002');
+});
